0c53ac1403c5abdb4f3b5d996139f6967e9ccf9e
mahdi <71930380@students.liu.edu.lb>
2024-06-04 11:43:26 +0300

    init my folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header1 from "components/1-header/Header1";
+import Header2 from "components/1-header/Header2";
+import Header3 from "components/1-header/Header3";
+import Hero from "components/2-hero/Hero";
+import Main from "components/3-main/Main";
+import Footer from "components/4-footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header1 />
+      <Header2 />
+      <Header3 />
+      <Hero />
+      <Main />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
