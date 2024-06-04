a4cc9a33ae8c2e759e68aa719f0614ba54e059ef
mahdi <71930380@students.liu.edu.lb>
2024-06-04 16:30:58 +0300

    Update palette colors value

--- src/theme.jsx
+++ src/theme.jsx
@@ -8,59 +8,29 @@ export const getDesignTokens = (mode) => ({
     ...(mode === "light"
       ? {
           // palette values for light mode
-          background: {
-            default: "#F9FAFB", // light gray for the default background
-            paper: "#FFFFFF", // white for paper elements
-          },
           text: {
-            primary: "#1F2937", // darker cool gray for primary text
-            secondary: "#4B5563", // medium cool gray for secondary text
-            disabled: "#9CA3AF", // light gray for disabled text
-          },
-          primary: {
-            main: "#3B82F6", // blue for primary elements like buttons
-          },
-          secondary: {
-            main: "#EF4444", // red for secondary elements like alerts
+            primary: "#2B3445",
           },
           neutral: {
-            main: "#5B728A", // cooler tone for neutral elements
+            main: "#64748B",
           },
-          accent: {
-            main: "#10B981", // green for accents and highlights
-          },
+
           favColor: {
-            main: "#E0E7FF", // light cool blue for favorite items
+            main: grey[300],
           },
-          divider: "#E5E7EB", // light gray for dividers
         }
       : {
           // palette values for dark mode
-          background: {
-            default: "#1F2937", // dark cool gray for the default background
-            paper: "#374151", // dark gray for paper elements
+          neutral: {
+            main: "#64748B",
+          },
+
+          favColor: {
+            main: grey[800],
           },
           text: {
-            primary: "#E0E7FF", // light cool blue for primary text
-            secondary: "#9CA3AF", // medium light gray for secondary text
-            disabled: "#6B7280", // darker gray for disabled text
+            primary: "#fff",
           },
-          primary: {
-            main: "#3B82F6", // blue for primary elements like buttons
-          },
-          secondary: {
-            main: "#EF4444", // red for secondary elements like alerts
-          },
-          neutral: {
-            main: "#9CA3AF", // light gray for neutral elements
-          },
-          accent: {
-            main: "#10B981", // green for accents and highlights
-          },
-          favColor: {
-            main: "#374151", // dark cool gray for favorite items
-          },
-          divider: "#4B5563", // dark gray for dividers
         }),
   },
   
